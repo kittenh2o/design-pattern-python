f91ddaa38c05017968173f274017b35226d56aa2
huai <yanghuai2012@outlook.com>
2019-04-05 22:18:20 +0800

    format code

--- main/src/factory.py
+++ main/src/factory.py
@@ -1,21 +1,26 @@
 from abc import abstractmethod, ABCMeta
 
+
 class Animal(metaclass=ABCMeta):
     @abstractmethod
     def make_sound(self):
         pass
 
+
 class Leopard(Animal):
     def make_sound(self):
         print("roar")
+
 
 class Panda(Animal):
     def make_sound(self):
         print("ah")
 
+
 class Aligator(Animal):
     def make_sound(self):
         print("huhh")
+
 
 class SimpleFactory:
     animals = {
@@ -24,23 +29,28 @@ class SimpleFactory:
         "aligator": Aligator
     }
 
-    def get_animal(self, animal_type:str):
+    def get_animal(self, animal_type: str):
         return SimpleFactory.animals[animal_type]
 
 # Abstract factory pattern
+
+
 class VegPizza(metaclass=ABCMeta):
     @abstractmethod
     def prepare(self):
         raise NotImplementedError
+
 
 class NonVegPizza(metaclass=ABCMeta):
     @abstractmethod
     def serve(self, VegPizza):
         raise NotImplementedError
 
+
 class DeluxeVeggiePizza(VegPizza):
     def prepare(self):
         print("Prepare {0}".format(type(self).__name__))
+
 
 class BaconPizza(NonVegPizza):
     def serve(self, veg_pizza):
@@ -54,15 +64,16 @@ class PizzaFactory(metaclass=ABCMeta):
     @abstractmethod
     def create_veg_pizza(self):
         raise NotImplementedError
-    
+
     @abstractmethod
     def create_non_veg_pizza(self):
         raise NotImplementedError
+
 
 class CanadianPizzaFactory(PizzaFactory):
 
     def create_veg_pizza(self):
         return DeluxeVeggiePizza()
-    
+
     def create_non_veg_pizza(self):
         return BaconPizza()
